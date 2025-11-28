a2085484412307d220c99067caf2d21d9e1e759a
Shen-Yukang <shenyukang99@gmail.com>
2025-11-27 22:18:02 -0500

    dist

--- my-project-page/vite.config.js
+++ my-project-page/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/my-project-page/',
+   base: '/yukang.github.io/',
   plugins: [
     tailwindcss(),
     react({

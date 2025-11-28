220ff1474516d7301b635de4f184ad668485f58d
Shen-Yukang <shenyukang99@gmail.com>
2025-11-27 22:26:51 -0500

    refine toute

--- my-project-page/src/main.jsx
+++ my-project-page/src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>  
-    <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/yukang.github.io">
+      <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 )
